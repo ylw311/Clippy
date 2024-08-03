e8900a5766d96e8fc245fe9661504663671453cb
Linda Wang <61102300+ylw311@users.noreply.github.com>
2024-08-03 17:36:16 -0400

    fix(events): right shift allowed

--- events/events.py
+++ events/events.py
@@ -12,11 +12,11 @@ from matlab.matlab import start
 
 FLAG = False
 # Detect if the platform is macOS or Windows
-IS_MAC = platform.system() == 'Darwin'
-IS_WINDOWS = platform.system() == 'Windows'
+IS_MAC = platform.system() == "Darwin"
+IS_WINDOWS = platform.system() == "Windows"
 # Detect if the platform is macOS or Windows
-IS_MAC = platform.system() == 'Darwin'
-IS_WINDOWS = platform.system() == 'Windows'
+IS_MAC = platform.system() == "Darwin"
+IS_WINDOWS = platform.system() == "Windows"
 
 # Define the CONTROL key based on the OS
 CONTROL = keyboard.Key.cmd if IS_MAC else keyboard.Key.ctrl
@@ -24,35 +24,42 @@ CONTROL = keyboard.Key.cmd if IS_MAC else keyboard.Key.ctrl
 # Define key combinations
 current_keys = set()
 
-CTRL_V_KEYS_WINDOWS = {'\x16'}  # Ctrl+V
-CTRL_V1_KEYS_WINDOWS = {'<49>'}  # Ctrl+V+1 using the combined representation
-CTRL_V2_KEYS_WINDOWS = {'<50>'}  # Ctrl+V+2 using the combined representation
-CTRL_V3_KEYS_WINDOWS = {'<51>'}  # Ctrl+V+3 using the combined representation
-TERMINATE_COMBINATION_WINDOWS = {'\x10'}  # Ctrl+P (use '\x10' for 'p')
+CTRL_V_KEYS_WINDOWS = {"\x16"}  # Ctrl+V
+CTRL_V1_KEYS_WINDOWS = {"<49>"}  # Ctrl+V+1 using the combined representation
+CTRL_V2_KEYS_WINDOWS = {"<50>"}  # Ctrl+V+2 using the combined representation
+CTRL_V3_KEYS_WINDOWS = {"<51>"}  # Ctrl+V+3 using the combined representation
+TERMINATE_COMBINATION_WINDOWS = {"\x10"}  # Ctrl+P (use '\x10' for 'p')
 
 # Define key combinations for macOS
-CTRL_V_KEYS_MAC = {CONTROL, 'v'}  # Cmd+V
-CTRL_V1_SHIFT_KEYS_MAC = {CONTROL, keyboard.Key.shift, 'v', '1'}  # Cmd+V+1+Shift
-TERMINATE_COMBINATION_MAC = {CONTROL, 'p'}  # Cmd+P
+CTRL_V_KEYS_MAC = {CONTROL, "v"}  # Cmd+V
+CTRL_V1_SHIFT_KEYS_MAC = {CONTROL, "v", "1"}  # Cmd+V+1
+SHIFT_KEYS = {keyboard.Key.shift, keyboard.Key.shift_r}  # Left and Right Shift
+TERMINATE_COMBINATION_MAC = {CONTROL, "p"}  # Cmd+P
 UNDO_KEY = "z"
 controller = keyboard.Controller()
+
 
 def undo():
     global FLAG
     FLAG = True  # Set the flag to prevent repeat execution
-    
+
     controller.press(CONTROL)
     controller.press(UNDO_KEY)
     controller.release(UNDO_KEY)
     controller.release(CONTROL)
 
+
 # Set up logging configuration
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+
 
 def show_paste_options():
     # Fetch and print clipboard content
     clipboard_content = pyperclip.paste()
     logging.info(f"Clipboard content: {clipboard_content}")
+
 
 def on_press(key):
     logging.debug(f"Key pressed: {key}")
@@ -66,56 +73,58 @@ def on_press(key):
 
     if not FLAG:
         if IS_WINDOWS:
-            print('windows')
+            print("windows")
             # Check for Ctrl+V
             if all(k in current_keys for k in CTRL_V_KEYS_WINDOWS):
-                logging.info('Ctrl+V pressed (Windows)')
-                show_paste_options()    
+                logging.info("Ctrl+V pressed (Windows)")
+                show_paste_options()
 
             # Check for Ctrl+V+1
             if all(k in current_keys for k in CTRL_V1_KEYS_WINDOWS):
-                logging.info('Ctrl+V+1 pressed (Windows)')
+                logging.info("Ctrl+V+1 pressed (Windows)")
                 # Handle specific case for Ctrl+V+1
-                logging.info('Special key combination Ctrl+V+1 triggered!')
+                logging.info("Special key combination Ctrl+V+1 triggered!")
                 undo()
                 start(pyperclip.paste(), logging)
-            
+
             # Check for Ctrl+V+2
             if all(k in current_keys for k in CTRL_V2_KEYS_WINDOWS):
-                logging.info('Ctrl+V+2 pressed (Windows)')
+                logging.info("Ctrl+V+2 pressed (Windows)")
                 # Handle specific case for Ctrl+V+2
-                logging.info('Special key combination Ctrl+V+2 triggered!')
+                logging.info("Special key combination Ctrl+V+2 triggered!")
 
             # Check for Ctrl+V+3
             if all(k in current_keys for k in CTRL_V3_KEYS_WINDOWS):
-                logging.info('Ctrl+V+3 pressed (Windows)')
+                logging.info("Ctrl+V+3 pressed (Windows)")
                 # Handle specific case for Ctrl+V+3
-                logging.info('Special key combination Ctrl+V+3 triggered!')
+                logging.info("Special key combination Ctrl+V+3 triggered!")
 
             # Check for terminating keys (Ctrl+P)
             if all(k in current_keys for k in TERMINATE_COMBINATION_WINDOWS):
-                logging.info('Ctrl+P pressed (Windows). Exiting...')
+                logging.info("Ctrl+P pressed (Windows). Exiting...")
                 os._exit(0)  # Terminate the script immediately
 
         elif IS_MAC:
-            print('macOS')
+            print("macOS")
             # Check for Cmd+V
             if all(k in current_keys for k in CTRL_V_KEYS_MAC):
-                logging.info('Cmd+V pressed (macOS)')
+                logging.info("Cmd+V pressed (macOS)")
                 show_paste_options()
 
-            # Check for Cmd+V+1
-            if all(k in current_keys for k in CTRL_V1_SHIFT_KEYS_MAC):
-                logging.info('Cmd+V+1+shift pressed (macOS)')
-                # Handle specific case for Cmd+V+1
-                logging.info('Special key combination Cmd+V+1 triggered!')
+            if all(k in current_keys for k in CTRL_V1_SHIFT_KEYS_MAC) and any(
+                k in current_keys for k in SHIFT_KEYS
+            ):
+                logging.info("Cmd+V+1+Shift pressed (macOS)")
+                # Handle specific case for Cmd+V+1+Shift
+                logging.info("Special key combination Cmd+V+1+Shift triggered!")
                 undo()
                 start(pyperclip.paste(), logging)
 
             # Check for terminating keys (Cmd+P)
             if all(k in current_keys for k in TERMINATE_COMBINATION_MAC):
-                logging.info('Cmd+P pressed (macOS). Exiting...')
+                logging.info("Cmd+P pressed (macOS). Exiting...")
                 os._exit(0)  # Terminate the script immediately
+
 
 def on_release(key):
     global FLAG
@@ -123,7 +132,7 @@ def on_release(key):
     logging.debug(f"Key released: {key}")
     try:
         # Remove the released key from the current set
-        if hasattr(key, 'char') and key.char is not None:
+        if hasattr(key, "char") and key.char is not None:
             current_keys.remove(key.char.lower())
         else:
             current_keys.remove(key)  # Ensure special keys are removed as key objects
@@ -132,6 +141,7 @@ def on_release(key):
         FLAG = False
     except KeyError:
         pass
+
 
 # Start the listener
 with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
