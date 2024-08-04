81de85c93848236b00768a4119c716f9807bb831
Edmond <liedmond0527@gmail.com>
2024-08-04 02:45:12 -0400

    fixed db

--- db/mongodb_vector_embedding.py
+++ db/mongodb_vector_embedding.py
@@ -1,28 +1,27 @@
 import os
-from pymongo import MongoClient
-# import numpy as np
+from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
-# from numpy.linalg import norm
 
-# # Load environment variables from .env file
+# Load environment variables from .env file
 load_dotenv()
 
+# Retrieve credentials from environment variables
 username = os.getenv("MONGODB_USERNAME")
 password = os.getenv("MONGODB_PASSWORD")
 database_name = os.getenv("DATABASE_NAME")
 collection_name = os.getenv("COLLECTION_NAME")
 
-url = f"mongodb+srv://{username}:{password}@{database_name}.iw7r6n4.mongodb.net/?retryWrites=true&w=majority&appName={collection_name}"
-print(url)
+def get_mongo_collection():
+    uri = f"mongodb+srv://{username}:{password}@hackthe6ix.iw7r6n4.mongodb.net/?retryWrites=true&w=majority&appName=Hackthe6ix"
+    print(uri)
 
-# Create a new client and connect to the server
-client = MongoClient(url)
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    # Send a ping to confirm a successful connection
+    try:
+        client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+get_mongo_collection()
 
-db = client[database_name]
-collection = db[collection_name]
