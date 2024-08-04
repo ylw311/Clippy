bf1f3878ac6b7ee25fc6d9d18eb48b9608dbae1d
Nathan <29968201+Nathan13888@users.noreply.github.com>
2024-08-04 08:50:27 +0000

    websocket communication
    Signed-off-by: Nathan <29968201+Nathan13888@users.noreply.github.com>

--- ui/src/app/main.tsx
+++ ui/src/app/main.tsx
@@ -4,27 +4,68 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { formatDistance, subSeconds } from "date-fns";
 import { AnimatePresence, motion } from "framer-motion";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRef, useState } from "react";
+import { RefreshCw } from "lucide-react";
 
-export function Main() {
-  const [history, setHistory] = useState<
-    {
+type Message =
+  | {
+      type: "text";
       message: string;
       timestamp: Date;
-    }[]
-  >([
+    }
+  | {
+      type: "choice";
+      message: { prompt: string; id: number }[];
+      disabled: boolean;
+      timestamp: Date;
+    };
+
+export function Main() {
+  // history
+  const [history, setHistory] = useState<Message[]>([
     {
+      type: "text",
       message:
         "hi.. lorem ipsum bruh smhhi.. lorem ipsum bruh smhhi.. lorem ipsum bruh smhhi.. lorem ipsum bruh smhhi.. lorem ipsum bruh smhhi.. lorem ipsum bruh smh",
       timestamp: subSeconds(new Date(), 3),
     },
+    {
+      type: "choice",
+      message: [
+        { prompt: "yes", id: 1 },
+        { prompt: "no", id: 2 },
+        { prompt: "HUGGGEEE PROMPT", id: 3 },
+        { prompt: ".............................", id: 4 },
+      ],
+      disabled: true,
+      timestamp: subSeconds(new Date(), 2),
+    },
   ]);
 
-  // scroll to bottom
+  // websocket to server
+  const url = "ws://localhost:8000/ws";
+  useEffect(() => {
+    const ws = new WebSocket(url);
 
+    // open
+    ws.addEventListener("open", () => {
+      console.log("connected to server");
+    });
+
+    // close
+    ws.addEventListener("close", () => {
+      console.log("disconnected from server");
+    });
+
+    // message
+    ws.addEventListener("message", (event) => {
+      console.log("received: ", event.data);
+    });
+  });
+
+  // (auto) scroll to bottom
   const messagesContainerRef = useRef<HTMLDivElement>(null);
-
   React.useEffect(() => {
     if (messagesContainerRef.current) {
       messagesContainerRef.current.scrollTop =
@@ -35,7 +76,7 @@ export function Main() {
   return (
     <div className="overflow-y-none mx-auto flex h-dvh max-w-3xl flex-col items-center justify-center space-y-4 px-8 py-4 text-white">
       {/* PROJECT NAME */}
-      <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+      <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-[5rem]">
         <span className="text-[hsl(280,100%,70%)]">Clippy ✂️</span>
       </h1>
 
@@ -72,11 +113,62 @@ export function Main() {
                 )}
               >
                 <div className="flex items-center gap-3">
-                  <span className="flex max-w-xs flex-col -space-y-1 rounded-md bg-gray-700 bg-opacity-75 p-3">
-                    <span className="text-background text-sm">
-                      {message.message}
-                    </span>
-                    <span className="text-background/50 align-self-bottom h-full text-end text-xs italic">
+                  <span
+                    className={cn(
+                      "flex max-w-xs flex-col rounded-md bg-gray-700 bg-opacity-75 p-3",
+                      message.type === "text" ? "-space-y-1" : "space-y-3",
+                    )}
+                  >
+                    {message.type === "text" ? (
+                      <span className="text-sm text-background">
+                        {message.message}
+                      </span>
+                    ) : (
+                      <div
+                        className={cn(
+                          "align-center grid h-fit items-center",
+                          // "space-y-2",
+                          // "space-x-4",
+                          "gap-4 auto-rows-auto",
+                        )}
+                      >
+                        {message.message.map((choice) => (
+                            <Button
+                              key={choice.id}
+                              variant="outline"
+                              className="bg-gray-700 h-[70px]"
+                              onClick={() => {
+                                setHistory([
+                                  ...history,
+                                  {
+                                    type: "text",
+                                    message: `You chose ${choice.prompt}`,
+                                    timestamp: new Date(),
+                                  },
+                                ]);
+                                // websocket.send()
+                              }}
+                            >
+                              {choice.prompt}
+                            </Button>
+                        ))}
+                        <span
+                          onClick={() =>
+                            setHistory([
+                              ...history,
+                              {
+                                type: "text",
+                                message: "Refreshing agent options...",
+                                timestamp: new Date(),
+                              },
+                            ])
+                          }
+                        >
+                          <RefreshCw className="h-[70px] w-[70px] rounded-sm border bg-gray-700 p-2" />
+                        </span>
+                      </div>
+                    )}
+                    <span className="align-self-bottom h-full text-end text-xs italic text-background/50">
                       {formatDistance(message.timestamp, new Date(), {
                         addSuffix: true,
                         includeSeconds: true,
@@ -89,21 +181,6 @@ export function Main() {
           </AnimatePresence>
         </div>
       </div>
-      <Button
-        onClick={() =>
-          setHistory([
-            ...history,
-            {
-              message: "hi",
-              timestamp: new Date(),
-            },
-          ])
-        }
-        className="w-full"
-        variant="default"
-      >
-        Add message
-      </Button>
     </div>
   );
 }
